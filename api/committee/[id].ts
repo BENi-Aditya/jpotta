1795feb89b6154314ac6887eb69471ea777ef554
Aditya Tripathi <aditya.tripathi.beni@gmail.com>
2026-04-17 21:06:45 +0530

    lets just try for once

--- api/committee/[id].ts
+++ api/committee/[id].ts
@@ -9,8 +9,6 @@ const cors = (res: VercelResponse) => {
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 };
 
-
-
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   cors(res);
   if (req.method === "OPTIONS") return res.status(200).end();
